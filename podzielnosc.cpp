be8e59715412a4d47973b9026c6ea1d29a4933d3
Piotr <mindapiotr@mat.umk.pl>
2025-11-24 16:56:20 +0100

    sprawdzam

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 using namespace std;
-int main(){
-    int a,b;
+
+int main() {
+    int a, b;
     cin >> a >> b;
+
+    // Dodane sprawdzanie
+    if (a <= 0 || b <= 0) {
+        cout << "Liczby musza byc dodatnie!" << endl;
+        return 0;
+    }
+
     cout << a << " " << b << endl;
     return 0;
 }
